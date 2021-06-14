70b282291ebe69e8d419c35b1de5c09fcc883cfc
Mohamed Hanafy <mohamed.hanfy.dev@outlook.com>
2021-06-14 18:29:14 +0200

    Add project files.

---
+++ App4/App4/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.CommunityToolkit.Extensions;
+using Xamarin.Forms;
+
+namespace App4
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            App.Current.MainPage.Navigation.ShowPopup(new MyPopup());
+        }
+    }
+}
